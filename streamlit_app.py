de9d8e27b4241ffe46e42f897b3fef3822e4fc1c
raniabr-git <rania.ben-romdhane@kpconsulting.fr>
2026-02-17 15:51:54 +0100

    Update streamlit_app.py
    chqnges : move SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,14 @@
 import streamlit as st
 from snowflake.snowpark.functions import col 
 
-
-from snowflake.snowpark.context import get_active_session
-
+cnx=st.connection("snowflake")
 option =st.selectbox('how you want to be contacted',('Email','Phone','Home phone'))
 st.write('you selected :', option)
 
 name_on_order = st.text_input('Name on smooth')
 st.write('name is :', name_on_order)
 
-session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
